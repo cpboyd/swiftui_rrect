dd02460c7dc93833cb0a83b48a4927a16048fafe
Christopher Boyd <xpboyd@gmail.com>
2021-10-18 18:05:13 -0400

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Christopher Boyd on 10/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/rrectApp.swift
@@ -0,0 +1,17 @@
+//
+//  rrectApp.swift
+//  Shared
+//
+//  Created by Christopher Boyd on 10/7/21.
+//
+
+import SwiftUI
+
+@main
+struct rrectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
